110d3bb4587dc36a9247977dda6b9f3e2adc61cc
yusiqo <80514373+yusiqo@users.noreply.github.com>
2022-01-09 16:28:33 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ pkg update
 pkg install git
 pkg install pip
 pkg install python
-echo -e '#!/bin/bash\ncd $PREFIX/lib/insta/\npython insta.py $1 $2' > $PREFIX/bin/insta
+echo -e '#!/bin/bash\ncd $PREFIX/lib/insta/\npython con.py $1 $2' > $PREFIX/bin/insta
 chmod 777 $PREFIX/bin/insta
 clear
